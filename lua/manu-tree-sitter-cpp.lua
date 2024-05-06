d15b35b02413f0e1e93a1a270e31d0077d00f212
Manuel Pedrozo <manupedr@gmail.com>
2024-05-06 22:54:04 +0200

    No whitespace to begin a template argument list

--- lua/manu-tree-sitter-cpp.lua
+++ lua/manu-tree-sitter-cpp.lua
@@ -6,7 +6,7 @@ M.setup = function()
         install_info = {
             url = "https://github.com/manupedrozo/tree-sitter-cpp",
             files = { "src/parser.c", "src/scanner.c" },
-            revision = "b2da5138ba6581c25f0a3bc84815db349195ebb3",
+            revision = "33ae6c75eadce9dc87e8575643a9dcef81e37682",
             generate_requires_npm = true,
         },
         maintainers = { "@manupedrozo" },
